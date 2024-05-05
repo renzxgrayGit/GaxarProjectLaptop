dd43f469f2d6b758df88a85d3de3b81091a597e3
renzxgrayGit <renzocabajar9@gmail.com>
2024-05-06 07:04:03 +0800

    updated gaxar

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -10,16 +10,16 @@ import headerLogo from './header-logo.jpg'
 
 import offer from './offer.svg'
 
-import shoe4 from './shoe4.svg'
-import shoe5 from './shoe5.svg'
-import shoe6 from './shoe6.svg'
-import shoe7 from './shoe7.svg'
+import shoe4 from './shoe4.png'
+import shoe5 from './shoe5.png'
+import shoe6 from './shoe6.png'
+import shoe7 from './shoe7.png'
 import shoe8 from './shoe8.svg'
 
-import thumbnailBackground from './thumbnail-background.svg'
-import thumbnailShoe1 from './thumbnail-shoe1.svg'
-import thumbnailShoe2 from './thumbnail-shoe2.svg'
-import thumbnailShoe3 from './thumbnail-shoe3.svg'
+import thumbnailBackground from './thumbnail-background.png'
+import thumbnailShoe1 from './big-shoe1.png'
+import thumbnailShoe2 from './big-shoe2.png'
+import thumbnailShoe3 from './big-shoe3.png'
 
 export {
     bigShoe1,
